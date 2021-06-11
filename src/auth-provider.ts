fa7e62e871de990fb97300c1741b7164faf89a62
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-11 14:41:38 +0800

    封装通用http请求方法

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -9,7 +9,7 @@ const localStorageKey = "__auth_provider_token__";
 export const getToken = () => window.localStorage.getItem(localStorageKey);
 
 export const handleUserResponse = ({ user }: { user: User }) => {
-    // window.localStorage.setItem(localStorageKey, user.token || "");
+    window.localStorage.setItem(localStorageKey, user.token || "");
     return user;
 };
 
