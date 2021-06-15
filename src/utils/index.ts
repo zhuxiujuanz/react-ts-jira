2810f5b77e64cbfee3619e62ed15e2ab8fbeb828
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-15 13:43:35 +0800

    去除警告信息

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,15 +2,25 @@ import { useEffect, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
+export const isVoid = (value: unknown) =>
+    value === undefined || value === null || value === "";
+
+// let a: object
+// a = {name: 'jack'}
+// a = () => {
+// }
+// a = new RegExp('')
+//
+// let b: { [key: string]: unknown }
+// b = {name: 'Jack'}
+// b = () => {}
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
     // Object.assign({}, object)
     const result = { ...object };
     Object.keys(result).forEach((key) => {
-        // @ts-ignore
         const value = result[key];
-        if (isFalsy(value)) {
-            // @ts-ignore
+        if (isVoid(value)) {
             delete result[key];
         }
     });
@@ -20,6 +30,8 @@ export const cleanObject = (object: object) => {
 export const useMount = (callback: () => void) => {
     useEffect(() => {
         callback();
+        // TODO 依赖项里加上callback会造成无限循环，这个和useCallback以及useMemo有关系
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 };
 
@@ -50,7 +62,7 @@ export const useMount = (callback: () => void) => {
 //             // 所以，log()#3 结束后，就只剩timeout#3在独自等待了
 
 // 后面用泛型来规范类型
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(() => {
@@ -63,18 +75,17 @@ export const useDebounce = (value: unknown, delay?: number): any => {
     return debouncedValue;
 };
 
-export const useArray = <T>(initialArray:T[])=>{
-    const [value, setValue] = useState((initialArray));
+export const useArray = <T>(initialArray: T[]) => {
+    const [value, setValue] = useState(initialArray);
     return {
         value,
         setValue,
-        add:(item:T)=>setValue([...value, item]),
-        clear:()=> setValue([]),
-        removeIndex:(index:number)=>{
+        add: (item: T) => setValue([...value, item]),
+        clear: () => setValue([]),
+        removeIndex: (index: number) => {
             const copy = [...value];
             copy.splice(index, 1);
-            setValue(copy)
-        }
-    }
-}
-
+            setValue(copy);
+        },
+    };
+};
