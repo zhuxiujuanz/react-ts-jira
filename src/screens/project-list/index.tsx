7982b690dc4364c106ee60173b59f92a1fc1fb13
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-15 10:50:29 +0800

    优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SearchPanel } from "screens/project-list/search-panel";
 import { List } from "screens/project-list/list";
-import { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-import * as qs from "qs";
-import {useHttp} from "../../utils/http";
+import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -12,38 +11,32 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
     const [users, setUsers] = useState([]);
+
     const [param, setParam] = useState({
         name: "",
         personId: "",
     });
     const debouncedParam = useDebounce(param, 200);
     const [list, setList] = useState([]);
-    const client = useHttp()
+    const client = useHttp();
+
     useEffect(() => {
-        client('projects', {data:cleanObject(debouncedParam)}).then(setList)
-        // fetch(
-        //     `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-        // ).then(async (response) => {
-        //     if (response.ok) {
-        //         setList(await response.json());
-        //     }
-        // });
+        client("projects", { data: cleanObject(debouncedParam) }).then(setList);
     }, [debouncedParam]);
 
     useMount(() => {
-        client('users' ).then(setUsers)
-
-        // fetch(`${apiUrl}/users`).then(async (response) => {
-        //     if (response.ok) {
-        //         setUsers(await response.json());
-        //     }
-        // });
+        client("users").then(setUsers);
     });
 
     return (
-        <div>
+        <Container>
+            <h1>项目列表</h1>
             <SearchPanel users={users} param={param} setParam={setParam} />
             <List users={users} list={list} />
-        </div>
+        </Container>
     );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,6 +1,7 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-import { Input, Select } from "antd";
-
+import { Form, Input, Select } from "antd";
+// 这个地方和老师讲解使用的不同，但是因为@emotion在React17的适配问题（@jsxImportSource/@jsxRuntime的作用）暂时替换成上面那种
 export interface User {
     id: string;
     name: string;
@@ -20,37 +21,37 @@ interface SearchPanelProps {
 }
 
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
-    return (
-        <form>
-            <div>
-                {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
-                <Input
-                    type="text"
-                    value={param.name}
-                    onChange={(evt) =>
-                        setParam({
-                            ...param,
-                            name: evt.target.value,
-                        })
-                    }
-                />
-                <Select
-                    value={param.personId}
-                    onChange={(value) =>
-                        setParam({
-                            ...param,
-                            personId: value,
-                        })
-                    }
-                >
-                    <Select.Option value={""}>负责人</Select.Option>
-                    {users.map((user) => (
-                        <Select.Option key={user.id} value={user.id}>
-                            {user.name}
-                        </Select.Option>
-                    ))}
-                </Select>
-            </div>
-        </form>
-    );
+    // @ts-ignore
+    return <Form css={{ marginBottom: "2rem" }} layout={"inline"}>
+        <Form.Item>
+            {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
+            <Input
+                placeholder={"项目名"}
+                type="text"
+                value={param.name}
+                onChange={(evt) =>
+                    setParam({
+                        ...param,
+                        name: evt.target.value,
+                    })
+                }
+            />
+        </Form.Item>
+        <Form.Item>
+            <Select
+                value={param.personId}
+                onChange={(value) =>
+                    setParam({
+                        ...param,
+                        personId: value,
+                    })
+                }
+            >
+                <Select.Option value={""}>负责人</Select.Option>
+                {users.map((user) => <Select.Option key={user.id} value={user.id}>
+                        {user.name}
+                    </Select.Option>)}
+            </Select>
+        </Form.Item>
+    </Form>;
 };
