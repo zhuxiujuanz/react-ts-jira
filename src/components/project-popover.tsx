878e64bbdf72614fa9e3d0e9eddd3f6fe780c964
zhuxiujuanz <zhuxj@teamsun.com.cn>
2021-06-23 18:19:38 +0800

    用redux toolkit改写同步状态管理

--- src/components/project-popover.tsx
+++ src/components/project-popover.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Divider, List, Popover, Typography } from "antd";
 import { useProjects } from "utils/project";
 import styled from "@emotion/styled";
-
-export const ProjectPopover = (props: { projectButton: JSX.Element }) => {
+import { ButtonNoPadding } from "components/lib";
+import { useDispatch } from "react-redux";
+import { projectListActions } from "screens/project-list/project-list.slice";
+export const ProjectPopover = () => {
+    const dispatch = useDispatch();
     const { data: projects, isLoading } = useProjects();
     const pinnedProjects = projects?.filter((project) => project.pin);
 
@@ -18,7 +21,12 @@ export const ProjectPopover = (props: { projectButton: JSX.Element }) => {
                 ))}
             </List>
             <Divider />
-            {props.projectButton}
+            <ButtonNoPadding
+                onClick={() => dispatch(projectListActions.openProjectModal())}
+                type={"link"}
+            >
+                创建项目
+            </ButtonNoPadding>
         </ContentContainer>
     );
 
